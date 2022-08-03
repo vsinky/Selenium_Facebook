9591490c24e7301d77cb941c0706eac756d7c847
vsinky <sinkykumariverma@gmail.com>
2022-08-03 19:41:41 +0530

    selenium listBox

--- src/test/java/com/bridgelabz/selenium/ListBox.java
+++ src/test/java/com/bridgelabz/selenium/ListBox.java
@@ -2,8 +2,10 @@ package com.bridgelabz.selenium;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.TreeSet;
 
 import org.openqa.selenium.By;
@@ -184,7 +186,32 @@ public class ListBox {
 		// driver.close();
 		 }
 		
-	
-	
-	
+//	Print the number of occurrance of Poori in the list box.
+	@Test
+	public void HashMapExample_printtheOcuuranceOfPoori() {
+		driver.get("file:///C:/Users/Admin/OneDrive/Desktop/ListBox_Breakfast.html");
+		WebElement list = driver.findElement(By.id("mtr"));
+		Select s = new Select(list);
+		List<WebElement> allElements = s.getOptions();
+		HashMap<String, Integer> hashMapObj = new HashMap<String, Integer>();
+		for (WebElement element : allElements) {
+			String text = element.getText();
+			if (hashMapObj.containsKey(text)) {
+			Integer value = hashMapObj.get(text);
+			value++;
+			hashMapObj.put(text, value);
+			}else{
+			hashMapObj.put(text, 1);
+			}
+			}
+			Set<String> allKeys = hashMapObj.keySet();
+			for (String key : allKeys) {
+			Integer value = hashMapObj.get(key);
+			System.out.println(key +" -->"+ value);
+			if (value>1) {
+			System.out.println("Occurance of " + key + " is :" + value);
+			}
+
+			}
+			}
 }
